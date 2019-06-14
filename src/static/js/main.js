3431e3a15f1a64785c0e70a968698fdcad35564b
Kirill <kotyunin.k@mail.ru>
2019-06-14 21:35:04 +0600

    Add features section

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,4 +1,13 @@
 $(function() {
-    //
-
+    $('.features-list').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 6000,
+        speed: 1100,
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        adaptiveHeight: false
+    });
 });
