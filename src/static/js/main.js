d961ffb462571d7ad55a2d65152c134b57537901
Kirill <kotyunin.k@mail.ru>
2019-06-15 21:06:56 +0600

    Add adaptive styles

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -8,6 +8,31 @@ $(function() {
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 3,
-        adaptiveHeight: false
+        adaptiveHeight: false,
+        responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+            }
+        }
+        ]
     });
 });
